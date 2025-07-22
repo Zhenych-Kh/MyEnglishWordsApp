dc0411a6bba35a1ad8ad92b77af1238ce88e3436
Zhenych-Kh <zhenych.kh@gmail.com>
2025-07-22 10:50:30 +0300

    -Introduced navigation-compose - Created Navigation.kt - Add screen SettingsScreen.kt - Add systempadding - Delete button Back

--- app/src/main/java/ua/ievetroy/myapplicationa/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/ua/ievetroy/myapplicationa/ui/screens/settings/SettingsScreen.kt
@@ -2,30 +2,26 @@ package ua.ievetroy.myapplicationa.ui.screens.settings
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import ua.ievetroy.myapplicationa.ui.theme.AppModifiers
 import ua.ievetroy.myapplicationa.ui.theme.AppTypography
 
 @Composable
 fun SettingsScreen(onBack: () -> Unit) {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+        modifier = AppModifiers.rootColumnModifier
+            .padding(top = 16.dp) // ← твій внутрішній паддінг
     ) {
         Text("Налаштування", style = AppTypography.wordTitle())
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Button(onClick = onBack) {
-            Text("Назад")
-        }
     }
 }
 
