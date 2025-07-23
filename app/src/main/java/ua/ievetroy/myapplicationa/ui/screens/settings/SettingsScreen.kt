c04054d8a75772101f0230f2a34046b44a74219f
Zhenych-Kh <zhenych.kh@gmail.com>
2025-07-23 11:44:35 +0300

    - Add icon exit button

--- app/src/main/java/ua/ievetroy/myapplicationa/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/ua/ievetroy/myapplicationa/ui/screens/settings/SettingsScreen.kt
@@ -1,16 +1,9 @@
 package ua.ievetroy.myapplicationa.ui.screens.settings
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import ua.ievetroy.myapplicationa.ui.theme.AppModifiers
 import ua.ievetroy.myapplicationa.ui.theme.AppTypography
